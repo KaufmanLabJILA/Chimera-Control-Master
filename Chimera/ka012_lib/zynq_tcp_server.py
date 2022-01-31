ac64234d97b06ffd1674327a974a4f85a447f11b
klab_Yb <alecjen@gmail.com>
2022-01-31 10:09:18 -0700

    sequencer working with python2

--- Chimera/ka012_lib/zynq_tcp_server.py
+++ Chimera/ka012_lib/zynq_tcp_server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-#from __future__ import print_function
 
 import socket
 import errno
@@ -42,33 +41,32 @@ class zynq_tcp_server:
 		sock.listen(1)
 
 		while True:
-			# Wait for a connection
-			print 'waiting for a connection'
-			connection, client_address = sock.accept()
+		    # Wait for a connection
+		    print 'waiting for a connection'
+		    connection, client_address = sock.accept()
 
-			try:
-				print 'connection from', client_address
+		    try:
+			    print 'connection from', client_address
 
-				# Receive the data in small chunks
-				while True:
-					data = connection.recv(64)
-					print '------------------------------------------------------------'
-					print 'received "%s"' % data
-					# print(len(data))
-					if data:
-						self.writeDevice(connection, data)
-						# connection.sendall(data)
-					else:
-						# print 'no more data from', client_address
-						break
-			except socket.error as error:
-				print error
-				break
+			    # Receive the data in small chunks
+			    while True:
+			        data = connection.recv(64)
+			        data = data.split('\0')[0]
+			        print 'received "%s"' % data
+			        # print(len(data))
+			        if data:
+			            self.writeDevice(connection, data)
+			            # connection.sendall(data)
+			        else:
+			            # print 'no more data from', client_address
+			            break
+		    except socket.error as error:
+		    	print error
+		    	break
 
 		connection.close()
 
 	def writeDevice(self, conn, data):
-		data = data.strip('\0')
 		data_split = data.split('_')
 		dev = data_split[0]
 		print 'dev = ', dev
@@ -90,10 +88,9 @@ class zynq_tcp_server:
 		elif (dev == 'DAC'):
 			self.seq.set_DAC(int(data_split[1]), float(data_split[2]))
 		elif (dev == 'DDS'):
-			if len(data_split)==3:
-				self.seq.set_DDS(int(data_split[1]), float(data_split[2]))
-			elif len(data_split)==4:
-				self.seq.set_DDS(int(data_split[1]), float(data_split[2]),float(data_split[3]))
+			self.seq.set_DDS(int(data_split[1]), float(data_split[2]))
+		elif (dev == 'lockPLL'):
+			self.seq.lock_PLL()
 		elif (dev == 'trigger'):
 			self.seq.soft_trigger()
 		else:
@@ -132,13 +129,13 @@ class zynq_tcp_server:
 		self.seq.dds_seq_write_points(self.ddsByteLen, byte_buf, num_snapshots)
 
 	def socket_read(self, conn, expected):
-		"""Read expected number of bytes from sock
-		Will repeatedly call recv until all expected data is received
-		"""
-		buffer = b''
-		while len(buffer) < expected:
-			buffer += conn.recv(expected - len(buffer))
-		return buffer
+	    """Read expected number of bytes from sock
+	    Will repeatedly call recv until all expected data is received
+	    """
+	    buffer = b''
+	    while len(buffer) < expected:
+	        buffer += conn.recv(expected - len(buffer))
+	    return buffer
 
 if __name__ == "__main__":
 	server = zynq_tcp_server()
